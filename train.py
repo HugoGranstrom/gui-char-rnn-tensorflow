25d572378348b365a1ef57212260b7f01fad482d
Hugo Granström <n15hugr@katedral.se>
2018-02-07 15:04:34 +0100

    Added basic GUI support through gooey in the training file

--- train.py
+++ train.py
@@ -10,6 +10,9 @@ from utils import TextLoader
 from model import Model
 
 
+from gooey import Gooey
+
+@Gooey
 def main():
     parser = argparse.ArgumentParser(
                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
